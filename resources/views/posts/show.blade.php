df806ef59bd0ddb17bba3d80a8c85c58888491fd
h-moto <h.motohashi1999@gmail.com>
2023-02-12 09:37:05 +0000

    8-5_編集画面の作成

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -19,6 +19,7 @@
                 <p>{{ $post->body }}</p>    
             </div>
 		</div>
+		<div class="edit"><a href="/posts/{{ $post->id }}/edit">edit</a></div>
         <div class="footer">
             <a href="/posts">戻る</a>
         </div>
